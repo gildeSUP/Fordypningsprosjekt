ee0f1118356ddfda31a9916ea2d36004936fef0e
gildeSUP <chritv@stud.ntnu.no>
2015-11-15 20:43:34 +0100

    implementing .STL-reader

---
+++ ReadSTL/ReadSTL/Header.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class read_STL
+{
+public:
+	read_STL(string fname);
+	void v3(char* facet);
+private:
+	vector<vector <double>> vec;
+};
+
+
+
+
+
---
+++ ReadSTL/ReadSTL/body.cpp
@@ -0,0 +1,81 @@
+#pragma once
+#include "Header.h"
+#include <vector>
+#include <iostream>
+#include <fstream>
+
+read_STL::read_STL(string fname)
+{
+	//!!
+	//don't forget ios::binary
+	//!!
+	ifstream myFile(
+		fname.c_str(), ios::in | ios::binary);
+
+	char header_info[80] = "";
+	char nTri[4];
+	unsigned long nTriLong;
+
+	//read 80 byte header
+	if (myFile) {
+		myFile.read(header_info, 80);
+		cout << "header: " << header_info << endl;
+	}
+	else {
+		cout << "error" << endl;
+	}
+
+	//read 4-byte ulong
+	if (myFile) {
+		myFile.read(nTri, 4);
+		nTriLong = *((unsigned long*)nTri);
+		cout << "n Tri: " << nTriLong << endl;
+	}
+	else {
+		cout << "error" << endl;
+	}
+
+	//now read in all the triangles
+	for (int i = 0; i < nTriLong; i++) {
+
+		char facet[50];
+
+		if (myFile) {
+
+			//read one 50-byte triangle
+			myFile.read(facet, 50);
+
+			//populate each point of the triangle
+			//using v3::v3(char* bin);
+			//facet + 12 skips the triangle's unit normal
+			v3(facet + 12);
+			v3(facet + 24);
+			v3(facet + 36);
+
+		}
+	}
+}
+
+void read_STL::v3(char* facet)
+{
+	char f1[4] = { facet[0],
+		facet[1],facet[2],facet[3] };
+
+	char f2[4] = { facet[4],
+		facet[5],facet[6],facet[7] };
+
+	char f3[4] = { facet[8],
+		facet[9],facet[10],facet[11] };
+
+	float xx = *((float*)f1);
+	float yy = *((float*)f2);
+	float zz = *((float*)f3);
+
+	vector<double> coords;
+	coords.push_back(double(xx));
+	coords.push_back(double(yy));
+	coords.push_back(double(zz));
+
+	vec.push_back(coords);
+
+}
