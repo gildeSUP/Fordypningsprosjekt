97dd9390050c5a2de44045b4727f7a8d9232f6d4
gildeSUP <chritv@stud.ntnu.no>
2015-11-16 16:31:48 +0100

    reading the stl file and stores it in vector

--- application/ReadSTL/ReadSTL/main.cpp
+++ application/ReadSTL/ReadSTL/main.cpp
@@ -2,5 +2,6 @@
 
 void main()
 {
-
+	string fname = "C:/Users/Christian/Documents/Visual Studio 2015/Projects/Fordypningsprosjekt/models/boundary geometry.STL";
+	read_STL open = read_STL(fname);
 }
