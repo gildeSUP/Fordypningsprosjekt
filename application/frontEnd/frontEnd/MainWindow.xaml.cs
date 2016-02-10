b92e7c9676d410fedb11fd1bee642dd6f043e889
gildeSUP <chritv@stud.ntnu.no>
2016-02-10 20:50:03 +0100

    create object class for validation object and change to use this class

--- application/frontEnd/frontEnd/MainWindow.xaml.cs
+++ application/frontEnd/frontEnd/MainWindow.xaml.cs
@@ -31,8 +31,8 @@ namespace frontEnd
 
         //obsobs: er i millimeter
         //List<Point3D> path = new List<Point3D> { new Point3D ( 1559.03, 160.589, 1274.49 ), new Point3D(1408.93, 8102.45, 1274.49), new Point3D(2001.82, 9093.03, 1274.49 ), new Point3D( 2742.6, 9614.62, 1274.49 ) };
-        List<Point3D> path = new List<Point3D>();
-        List<Point3D> newPath = new List<Point3D>();
+        private List<Point3D> path = new List<Point3D>();
+        private validationObject valObj;
 
        [DllImport("../../../../CppClassDll/Debug/CppClassDll.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern void openStl(string fname);
@@ -113,11 +113,11 @@ namespace frontEnd
 
         private Boolean iteratePath(List<Point3D> path)
         {
-            
-            newPath.Add(path[0]);
+            valObj = new validationObject();
+            valObj.newPath.Add(path[0]);
             foreach (Point3D node in path)
             {
-                testData.Items.Add("previousNode: " + newPath.Last().X + ", " + newPath.Last().Y + ", " + newPath.Last().Z);
+                testData.Items.Add("previousNode: " + valObj.newPath.Last().X + ", " + valObj.newPath.Last().Y + ", " + valObj.newPath.Last().Z);
                 testData.Items.Add("nextNode: " + node.X + ", " + node.Y + ", " + node.Z);
                 if (dynamicRelaxation(node)) { 
                     testData.Items.Add("dynamic relaxation done for this node");
@@ -139,31 +139,26 @@ namespace frontEnd
             Vector3D velocity = new Vector3D(0.0, 0.0, 0.0);
             Vector3D acceleration = new Vector3D(0.0, 0.0, 0.0);
 
-            double KObject = 0.1;
-            double CObject = 0.85; //2*Math.sqrt(m*sum(K))
-            
-            double mass = 5;
             double deltaT = 0.8; //0.1 during clash
-            Point3D N = newPath.Last(); //newPath[i]
-            displacement.X = N.X - node.X;
-            displacement.Y = N.Y - node.Y;
-            displacement.Z = N.Z - node.Z;
+            
+            displacement.X = valObj.getCurrentPossition().X - node.X;
+            displacement.Y = valObj.getCurrentPossition().Y - node.Y;
+            displacement.Z = valObj.getCurrentPossition().Z - node.Z;
             Boolean first = true;
 
             while (first==true || residualForce.Length>10) {
                 first=false;
 
-                residualForce = residualForce*alpha+(1.0-alpha)*(clashForce-KObject*displacement-CObject*velocity);
-
-                acceleration = residualForce/mass;
+                residualForce = residualForce*alpha+(1.0-alpha)*(clashForce-valObj.K*displacement-valObj.C*velocity);
+                acceleration = residualForce / valObj.mass;
                 velocity = acceleration*deltaT;
                 displacement += velocity * deltaT;
 
                 testData.Items.Add("D: " + displacement.Length + "      R: " + residualForce.Length + "      V: " + velocity.Length);
                 
             }
-            
-            newPath.Add(node);
+
+            valObj.newPath.Add(node); //change node to the actuall new position from displacement
             return true;
         }
 
---
+++ application/frontEnd/frontEnd/validationObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace frontEnd
+{
+    class validationObject
+    {
+        public double K { get; set; }
+        public double C { get; set; }
+        public double mass { get; set; }
+        public List<Point3D> newPath = new List<Point3D>();
+
+        public int lol { get; set; }
+
+        public validationObject()
+        {
+            K = 0.1;
+            C = 0.85; //2*Math.sqrt(m*sum(K))
+            mass = 5;
+        }
+        public Point3D getCurrentPossition()
+        {
+            return newPath.Last();
+        }
+    }
+}
